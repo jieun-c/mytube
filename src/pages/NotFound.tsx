be5710d4c73bbe5d587c645cb571752dc88099c5
jieun-c <flffl1231@naver.com>
2023-04-26 14:51:25 +0900

    make 404page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
-  return <div>404 Not Found</div>;
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <div className="bg-white px-10 py-10 rounded shadow-md max-w-fit w-11/12">
+        <p className="font-bold pb-5">404 Not Found.. 🥺</p>
+        <Link to="/" className="text-xs text-blue-600 underline">
+          Go to Mytube →
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
