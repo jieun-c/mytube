62137f0cd3e3afd5431e5fcd9da7a7e577e99c5f
jieun-c <flffl1231@naver.com>
2023-04-28 15:20:33 +0900

    add basename

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,30 @@ import VideoList from "./pages/VideoList";
 import VideoDetail from "./pages/VideoDetail";
 import NotFound from "./pages/NotFound";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    errorElement: <NotFound />,
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <VideoList />,
-      },
-      {
-        path: "/search",
-        element: <VideoList />,
-      },
-      {
-        path: "/detail/:videoId",
-        element: <VideoDetail />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      errorElement: <NotFound />,
+      element: <MainLayout />,
+      children: [
+        {
+          index: true,
+          element: <VideoList />,
+        },
+        {
+          path: "/search",
+          element: <VideoList />,
+        },
+        {
+          path: "/detail/:videoId",
+          element: <VideoDetail />,
+        },
+      ],
+    },
+  ],
+  { basename: "/mytube/" }
+);
 
 function App() {
   return <RouterProvider router={router} />;
